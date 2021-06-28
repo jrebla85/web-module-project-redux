153185cd36288cb3add53e76582bcb08361f105b
jeremy black <jrebla85@yahoo.com>
2021-06-27 23:13:22 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider} from 'react-redux';
-
 import reducer from './reducers';
-
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const store = createStore(reducer);
 ReactDOM.render(
+<Provider store={store}>
   <Router>
-      <App />
-  </Router>,
+    <App />
+  </Router>
+</Provider>,
   document.getElementById('root')
 );
